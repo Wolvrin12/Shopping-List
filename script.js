6cf2c3f655872caa8713173260c0ca3adbc53a64
Wolvrin12 <anouarchlih31@gmail.com>
2023-07-16 14:14:21 +0100

    Delete Shoped Items feature

--- script.js
+++ script.js
@@ -9,17 +9,20 @@ function inputLength() {
 function createListElement() {
   var li = document.createElement("li");
   li.appendChild(document.createTextNode(input.value));
+  li.addEventListener("click", () => {
+    ul.removeChild(li);
+  });
   ul.appendChild(li);
   input.value = "";
 }
 
-button.addEventListener("click", function () {
+button.addEventListener("click", () => {
   if (inputLength() > 0) {
     createListElement();
   }
 });
 
-input.addEventListener("keypress", function (e) {
+input.addEventListener("keypress", (e) => {
   if (inputLength() > 0 && e.which === 13) {
     createListElement();
   }
