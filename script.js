c3b886951cae7e75b03e1c869d5c11dd104a162a
Wolvrin12 <anouarchlih31@gmail.com>
2023-07-19 14:55:24 +0100

    Fixing spaces issue

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const button = document.getElementById("enter");
 const ul = document.querySelector("ul");
 const removeBtn = `<i class="fa fa-times" aria-hidden="true"></i>`;
 
-function createListElement() {
+const createListElement = () => {
   const li = document.createElement("li");
   li.appendChild(document.createTextNode(input.value));
   li.insertAdjacentHTML("beforeend", removeBtn);
@@ -12,16 +12,25 @@ function createListElement() {
   });
   ul.appendChild(li);
   input.value = "";
-}
+};
+
+const removeSpaces = (spacedValue) => {
+  const removedSpacesValue = spacedValue.split(" ").join("");
+  if ((removedSpacesValue.length = "")) {
+    return removedSpacesValue;
+  } else {
+    return input.value;
+  }
+};
 
 button.addEventListener("click", () => {
-  if (input.value.length > 0) {
+  if (removeSpaces(input.value).length > 0) {
     createListElement();
   }
 });
 
 input.addEventListener("keypress", (e) => {
-  if (input.value.length > 0 && e.which === 13) {
+  if (e.which === 13 && removeSpaces(input.value).length > 0) {
     createListElement();
   }
 });
